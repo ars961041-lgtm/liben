77f5830efc63856220be4b216f42618e7a340203
ars961041-lgtm <ars961041@gmail.com>
2026-03-27 11:50:24 +0300

    Prepare project for Render deployment

--- core/config.py
+++ core/config.py
@@ -1,8 +1,10 @@
+import os
 import time
 import threading
 import sqlite3
 
-TOKEN = '8725629234:AAEFcPRew6hOhDfcSRVTCB90L1C1cTDzp0k'
+TOK = os.environ.get("BOT_TOKEN")
+
 DB_PATH = 'database/group_responses.db'
 
 developers_id = {7632471789}
