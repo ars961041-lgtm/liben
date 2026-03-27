4c08fbd444f883450a417789adca332aade923fa
ars961041-lgtm <ars961041@gmail.com>
2026-03-28 02:30:01 +0300

    edit some mistakes 1

--- core/config.py
+++ core/config.py
@@ -3,11 +3,11 @@ import time
 import threading
 import sqlite3
 
-# TOKEN = os.environ.get("BOT_TOKEN")
-# is_test = False
+TOKEN = os.environ.get("BOT_TOKEN")
+is_test = False
 
-TOKEN = '8783291040:AAHK1DNtZNYA2lfnQqwjdX2cOvZhcO4MUuo'
-is_test = True
+# TOKEN = '8783291040:AAHK1DNtZNYA2lfnQqwjdX2cOvZhcO4MUuo'
+# is_test = True
 
 developers_id = {7632471789}
 
